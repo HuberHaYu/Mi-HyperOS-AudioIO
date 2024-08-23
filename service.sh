d63df33563dd02cc4ced74dad5b7a2b66e3411cf
Huber_HaYu <104569900+HuberHaYu@users.noreply.github.com>
2024-08-23 09:54:29 +0800

    Add files via upload

---
+++ service.sh
@@ -0,0 +1,27 @@
+#!/system/bin/sh
+alias audio_chip_set='tinymix'
+v=91
+vh=19
+MODDIR=${0%/*}
+wait_until_login() {
+    while [ "$(getprop sys.boot_completed)" != "1" ]; do
+        sleep 1
+    done
+    while [ ! -d "/sdcard/Android" ]; do
+        sleep 1
+    done
+    sleep 0.5s
+    base64 $MODDIR/system/vendor/etc/dolby/dax-default.xml > $MODDIR/system/vendor/etc/dolby/DD3XQA
+    rm $MODDIR/system/vendor/etc/dolby/dax-default.xml
+    mkdir -p $MODDIR/system/vendor/etc/vendor/lib/key_event/data/vendor/etc/mi/haptic
+    NA=$MODDIR/system/vendor/etc/vendor/lib/key_event/data/vendor/etc/mi/haptic
+    cp -f $MODDIR/system/vendor/etc/dolby/DD3XQA $NA
+    rm $MODDIR/system/vendor/etc/dolby/DD3XQA
+    mkdir -p $MODDIR/system/dolby
+    #monitor
+    while true; do
+        . $MODDIR/huber.sh
+        sleep 10
+    done
+}
+wait_until_login
